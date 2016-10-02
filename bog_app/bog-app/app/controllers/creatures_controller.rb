48a340c7ff4e881cdd8cbe426d2aa5967b1d694e
Lisa Olson <olson.lisa94@gmail.com>
2016-10-01 18:20:33 -0700

    All RESTful routing working

--- bog_app/bog-app/app/controllers/creatures_controller.rb
+++ bog_app/bog-app/app/controllers/creatures_controller.rb
@@ -9,7 +9,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
       if creature.save
         redirect_to creatures_path
@@ -19,6 +18,30 @@ class CreaturesController < ApplicationController
   def show
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
