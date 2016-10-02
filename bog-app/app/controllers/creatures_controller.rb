5ca3f7dada646e03b29c896de13bdf89e87c9e14
Lisa Olson <olson.lisa94@gmail.com>
2016-10-02 14:52:25 -0700

    Edit/Update working

--- bog-app/app/controllers/creatures_controller.rb
+++ bog-app/app/controllers/creatures_controller.rb
@@ -9,7 +9,7 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
+
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
@@ -22,4 +22,23 @@ class CreaturesController < ApplicationController
 
   end
 
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+    params.require(:creature).permit(:name, :description)
+  end
 end
